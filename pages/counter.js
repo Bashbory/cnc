68f5415a7dfba814909f9bf9ef2dcea994809f3b
Bashbory <eightmick912@gmail.com>
2023-06-11 11:53:26 +0300

    counter

--- pages/counter.js
+++ pages/counter.js
@@ -13,3 +13,21 @@ window.onscroll = function () {
 function goToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+
+
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const increment = target / 400;
+    if (count < target) {
+      counter.innerText = `${Math.ceil(count + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else counter.innerText = target;
+  };
+  updateCounter();
+});
